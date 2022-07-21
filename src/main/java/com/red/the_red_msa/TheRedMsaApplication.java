fee3338f74486a95d1da54f13534bf42c76c7d50
lyutvs <lyutvs@gmail.com>
2022-07-21 22:32:09 +0900

    Initial commit

---
+++ src/main/java/com/red/the_red_msa/TheRedMsaApplication.java
@@ -0,0 +1,13 @@
+package com.red.the_red_msa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRedMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheRedMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/red/the_red_msa/TheRedMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red.the_red_msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRedMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
